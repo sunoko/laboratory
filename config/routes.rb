6a385ae5d917b2d123dc712e3aae93e3cfc55e41
kazuki chinone <kazukichinone@kazukinoMacBook-Pro.local>
2018-12-26 18:48:29 +0900

    4-8

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   resources :users, only: %i[show]
   resources :listings
 
-  get 'manage-listing/:id/basics' => 'listings#basics', as: 'manage-listing-basics'
+  get 'manage-listing/:id/basics' => 'listings#basics', as: 'manage_listing_basics'
+  get 'manage-listing/:id/description' => 'listings#description', as: 'manage_listing_description'
+  get 'manage-listing/:id/address' => 'listings#address', as: 'manage_listing_address'
+  get 'manage-listing/:id/price' => 'listings#price', as: 'manage_listing_price'
+  get 'manage-listing/:id/photos' => 'listings#photos', as: 'manage_listing_photos'
+  get 'manage-listing/:id/calendar' => 'listings#calendar', as: 'manage_listing_calendar'
+  get 'manage-listing/:id/bankaccount' => 'listings#bankaccount', as: 'manage_listing_bankaccount'
+  get 'manage-listing/:id/publish' => 'listings#publish', as: 'manage_listing_publish'
 end
